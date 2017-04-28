7adba486e693be3682acedb40207246bc820d80d
admin <ryleysill93@gmail.com>
2017-04-27 22:37:57 -0700

    got logo working

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -23,16 +23,22 @@ class Main extends React.Component {
 
   render () {
     return (
-      <div>
-        <iframe
-          src={`http://player.twitch.tv/?channel=${this.state.channel}&muted=${this.state.muted}`}
-          height="558"
-          width="992"
-          frameBorder="0"
-          scrolling="no"
-          allowFullScreen="true">
-        </iframe>
-        <button className="button" onClick={this.getNewChannel}>random</button>
+      <div className="main">
+        <div className="header">
+          <img src="http://res.cloudinary.com/dwqeotsx5/image/upload/c_crop,h_213,w_1920/v1493350430/L5zqHSoxhY6x7mWekDPw0x0_jLe02pyoVX-t2HJi9sw_zq79zc.png" />
+          <h1>Twitch Roulette</h1>
+        </div>
+        <div className="content">
+          <iframe
+            src={`http://player.twitch.tv/?channel=${this.state.channel}&muted=${this.state.muted}`}
+            height="405"
+            width="720"
+            frameBorder="0"
+            scrolling="no"
+            allowFullScreen="true">
+          </iframe>
+          <button className="button" onClick={this.getNewChannel}>random</button>
+        </div>
       </div>
     );
   }
