97bffdda3c8e5b5ca2ca64c739d17a67a417bb5f
admin <ryleysill93@gmail.com>
2017-05-17 16:18:21 -0700

    refactored store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,6 +2,8 @@ import rootReducer from '../reducers/root_reducer';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const configureStore = (preloadedState = {}) => createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+const configureStore = (preloadedState = {}) => {
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+};
 
 export default configureStore;
